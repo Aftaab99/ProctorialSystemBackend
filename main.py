d9542bfe4742d89c48f9d7571c1258d0d8d520a5
Aftaab99 <aftaabzia9@gmail.com>
2019-11-05 22:26:03 +0530

    Added Postgres db connection and create queries

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
 if __name__ == '__main__':
+
+    conn = psycopg2.connect(database="postgres", user="postgres", password="aftaab", host="127.0.0.1", port="5432")
+    cursor = conn.cursor()
+    print("Database opened successfully")
+    with open('queries/create_query.sql') as query_file:
+        q = query_file.read()
+        cursor.execute(q)
+    conn.commit()
     app.run(debug=True)
