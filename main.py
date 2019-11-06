28a0f5f790f92dc890152d7cf517f3949ef38925
Aftaab99 <aftaabzia9@gmail.com>
2019-11-07 01:53:10 +0530

    Completed admin page and credential creation

--- main.py
+++ main.py
@@ -4,10 +4,32 @@ import psycopg2
 app = Flask(__name__)
 
 
+def get_db_connection():
+    conn = psycopg2.connect(database="postgres", user="postgres",
+                            password="aftaab", host="127.0.0.1", port="5432")
+    return conn
+
 @app.route('/')
 def hello():
     return "Index page"
 
+@app.route('/admin/add_proctor_cred', methods=['POST'])
+def add_proctor_cred():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    conn = get_db_connection()
+    q_add_cred = "INSERT INTO ProctorCredentials VALUES(%(proctor_id)s, %(password)s) ON CONFLICT(proctor_id) DO UPDATE SET password=%(password)s"
+    cursor = conn.cursor()
+    try:
+        cursor.execute(q_add_cred, {'proctor_id': email, 'password': password})
+        conn.commit()
+        conn.close()
+        return jsonify({'error': False})
+    except Exception as e:
+        print(e)
+        conn.commit()
+        conn.close()
+        return jsonify({'error': True})
 
 @app.route('/admin/login')
 def admin_login():
@@ -16,15 +38,31 @@ def admin_login():
 
 @app.route('/admin', methods=['GET'])
 def admin():
-    return "This is the admin page"
+    q_faculty_details = "SELECT name, faculty_id from Faculty"
+    q_proctor_ids = "SELECT distinct proctor_id from ProctorCredentials"
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(q_faculty_details)
+    fne = cursor.fetchall()
+    cursor.execute(q_proctor_ids)
+    proctor_ids =cursor.fetchall()
+    proctor_ids = [a[0] for a in proctor_ids]
+    conn.close()
+    print(fne)
+    faculty_data = [(fname,fid,fid in proctor_ids) for fname, fid in fne]
+    print(faculty_data)
+    print(proctor_ids)
+    
+    return render_template('admin_page.html', faculty_data=faculty_data)
+
+
 
 
 @app.route('/admin/checkpassword', methods=['POST'])
 def checkpassword():
     entered_password = request.form.get('password')
 
-    conn = psycopg2.connect(database="postgres", user="postgres",
-                            password="aftaab", host="127.0.0.1", port="5432")
+    conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT admin_password from Admin")
     hashed_password = cursor.fetchone()[0]
@@ -37,11 +75,16 @@ def checkpassword():
 
 if __name__ == '__main__':
 
-    conn = psycopg2.connect(database="postgres", user="postgres",
-                            password="aftaab", host="127.0.0.1", port="5432")
+    conn = get_db_connection()
     cursor = conn.cursor()
     print("Database opened successfully")
     with open('queries/create_query.sql') as query_file:
+        q = query_file.read()
+        cursor.execute(q)
+    with open('queries/add_department_data.sql') as query_file:
+        q = query_file.read()
+        cursor.execute(q)
+    with open('queries/add_faculty_data.sql') as query_file:
         q = query_file.read()
         cursor.execute(q)
     conn.commit()
--- queries/create_query.sql
+++ queries/create_query.sql
@@ -4,11 +4,16 @@ CREATE TABLE IF NOT EXISTS Student(student_usn VARCHAR(10) CHECK (student_usn ~ 
 
 CREATE TABLE IF NOT EXISTS Parent(parent_id VARCHAR(15) PRIMARY KEY, student_usn VARCHAR(15) REFERENCES Student(student_usn) ON DELETE CASCADE, first_name VARCHAR(20), middle_name VARCHAR(20), last_name VARCHAR(20), contact_NUMERIC NUMERIC(10), email_id VARCHAR(320));
 
-CREATE TABLE IF NOT EXISTS Faculty(faculty_id VARCHAR(15) PRIMARY KEY, department_id VARCHAR(15) REFERENCES Department(department_id) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS Faculty(faculty_id VARCHAR(320) PRIMARY KEY, name VARCHAR(50), department_id VARCHAR(15) REFERENCES Department(department_id) ON DELETE CASCADE);
 
-CREATE TABLE IF NOT EXISTS Proctor(proctor_id VARCHAR(15), student_usn VARCHAR(15), PRIMARY KEY(proctor_id,student_usn), FOREIGN KEY(proctor_id) REFERENCES Faculty(faculty_id) ON DELETE CASCADE, FOREIGN KEY(student_usn) REFERENCES Student(student_usn) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS Proctor(proctor_id VARCHAR(320), student_usn VARCHAR(15), PRIMARY KEY(proctor_id,student_usn), FOREIGN KEY(proctor_id) REFERENCES Faculty(faculty_id) ON DELETE CASCADE, FOREIGN KEY(student_usn) REFERENCES Student(student_usn) ON DELETE CASCADE);
 
-CREATE TABLE IF NOT EXISTS Messages(sender_id VARCHAR(15), receiver_id VARCHAR(15), sent_time TIMESTAMP, message_text VARCHAR(1000), PRIMARY KEY(sender_id, receiver_id, sent_time));
+CREATE TABLE IF NOT EXISTS Messages(sender_id VARCHAR(320), receiver_id VARCHAR(320), sent_time TIMESTAMP, message_text VARCHAR(1000), PRIMARY KEY(sender_id, receiver_id, sent_time));
 
-CREATE TABLE IF NOT EXISTS Reports(proctor_id VARCHAR(15), student_usn VARCHAR(15), meet_date DATE, report_file_name VARCHAR(25), PRIMARY KEY(proctor_id,student_usn,meet_date), FOREIGN KEY(proctor_id,student_usn) REFERENCES Proctor(proctor_id,student_usn) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS Reports(proctor_id VARCHAR(320), student_usn VARCHAR(15), meet_date DATE, report_file_name VARCHAR(25), PRIMARY KEY(proctor_id,student_usn,meet_date), FOREIGN KEY(proctor_id,student_usn) REFERENCES Proctor(proctor_id,student_usn) ON DELETE CASCADE);
 
+CREATE TABLE IF NOT EXISTS Admin(admin_password varchar(50));
+INSERT INTO Admin VALUES('ef5c965c33d32e9939d9f31670d3aa82') ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS ProctorCredentials(proctor_id VARCHAR(320) UNIQUE REFERENCES Faculty(faculty_id) ON DELETE CASCADE, password VARCHAR(512));
+CREATE TABLE IF NOT EXISTS StudentCredentials(student_usn VARCHAR(10) UNIQUE REFERENCES Student(student_usn) ON DELETE CASCADE, password VARCHAR(512));
